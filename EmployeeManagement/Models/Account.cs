1cbf9051176e6268b902d59220ebe6a5ffde227f
Nils Part <Nils.Part@protonmail.com>
2021-10-08 01:29:56 +0200

    Skapat 3 klasser i Models, Account, Admin, User.

---
+++ EmployeeManagement/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement.Models
+{
+    class Account
+    {
+        string Employee_Adress { get; set; }
+        int Employee_Id { get; set; }
+        string Employee_Name { get; set; }
+        string Employee_Position { get; set; }
+        
+    }
+}
